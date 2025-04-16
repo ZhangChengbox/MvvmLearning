75311a6a9f994aff16c813cb7ef91c121c75fc8d
ZhangChengbo <iChengbo@qq.com>
2025-04-16 22:40:27 +0800

    _title = "Hello, Zcb!";

---
+++ WpfApp1/WpfApp1/ViewModelBase.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using WpfApp1;
+
+public class MainWindowViewModel : ObservableObject
+{
+    private string _title = "Hello, Zcb!";
+
+    public MainWindowViewModel()
+    {
+        ButonClickCommand = new RelayCommand(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        ;
+    }
+
+    public string Title
+    {
+        get { return _title; }
+        set
+        {
+            if (_title != value)
+            {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+    }
+
+    private string _content = "Welcome to WPF!";
+
+    public string Content
+    {
+        get { return _content; }
+        set => SetProperty(ref _content, value);
+    }
+
+    public RelayCommand ButonClickCommand { get; }
+}
